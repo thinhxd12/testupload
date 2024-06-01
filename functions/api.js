323e6a76de378cea0a7783baa0b471c01b2e379e
thinhxd12 <thinhxd12@gmail.com>
2024-06-01 20:26:15 +0700

    server826

--- functions/api.js
+++ functions/api.js
@@ -2,13 +2,61 @@ import express from "express";
 import ServerlessHttp from "serverless-http";
 
 const app = express();
+const cors = require('cors');
+const Gtts = require('gtts');
+
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://hoctuvung3.vercel.app/'],
+    optionsSuccessStatus: 200
+}
 
 
-app.get('/.netlify/functions/api', (req, res) => {
-    return res.json({
-        messages: "hello world!"
-    })
-})
+
+// app.get('/.netlify/functions/api', (req, res) => {
+//     return res.json({
+//         messages: "hello world!"
+//     })
+// })
+
+
+const translate = require('google-translate-extended-api');
+const defaultTransOptions = {
+    returnRawResponse: false,
+    detailedTranslations: true,
+    definitionSynonyms: false,
+    detailedTranslationsSynonyms: true,
+    definitions: false,
+    definitionExamples: false,
+    examples: false,
+    removeStyles: false
+}
+
+const getExampleTransOptions = {
+    returnRawResponse: false,
+    detailedTranslations: false,
+    definitionSynonyms: false,
+    detailedTranslationsSynonyms: false,
+    definitions: false,
+    definitionExamples: false,
+    examples: true,
+    removeStyles: false
+}
+
+app.get('/trans', cors(corsOptions), async (req, res) => {
+    const { text, from, to } = req.query;
+
+    const result = await translate(text, from, to, defaultTransOptions);
+    return res.json(result)
+});
+
+app.get('/hear', cors(corsOptions), function (req, res) {
+    const gtts = new Gtts(req.query.text, req.query.lang);
+    gtts.stream().pipe(res);
+});
+
+app.get('/wakeup', cors(corsOptions), function (req, res) {
+    res.json({ res: 'it worked!' });
+});
 
 
 const handler = ServerlessHttp(app);
