e1651a4dcf3f1be4f73aa119d0718fd99e5a34f1
thinhxd12 <thinhxd12@gmail.com>
2024-06-01 22:05:53 +0700

    server1005

--- functions/api.js
+++ functions/api.js
@@ -2,8 +2,14 @@ import express from "express";
 import ServerlessHttp from "serverless-http";
 import Gtts from "gtts"
 import translate from "google-translate-extended-api"
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://hoctuvung3.vercel.app/'],
+    optionsSuccessStatus: 200
+}
 
 app.get('/api/wakeup', (req, res) => {
     res.json({ res: 'it worked!' });
@@ -20,25 +26,17 @@ const defaultTransOptions = {
     removeStyles: false
 }
 
-app.get('/api/trans', async (req, res) => {
+app.get('/api/trans', cors(corsOptions), async (req, res) => {
     const { text, from, to } = req.query;
     const result = await translate(text, from, to, defaultTransOptions);
     return res.json(result)
 })
 
-app.get('/api/hear', async (req, res) => {
+app.get('/api/hear', cors(corsOptions), async (req, res) => {
     const gtts = new Gtts(req.query.text, req.query.lang);
     gtts.stream().pipe(res);
 })
 
-import cors from 'cors';
-
-app.use(cors({
-    credentials: true,
-    preflightContinue: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    origin: true
-}));
 
 const handler = ServerlessHttp(app);
 
