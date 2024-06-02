dac84154f3cea5370d82aeaabb6ce3deb2ef7483
thinhxd12 <thinhxd12@gmail.com>
2024-06-02 08:40:28 +0700

    server840

--- api/index.ts
+++ api/index.ts
@@ -4,15 +4,10 @@ const translate = require('google-translate-extended-api');
 const Gtts = require('gtts');
 
 const app = express();
-
 const corsOptions = {
     origin: ['http://localhost:3000', 'http://localhost:5500', 'https://hoctuvung3.vercel.app'],
     optionsSuccessStatus: 200
 }
-
-app.get('/api/wakeup', (req, res) => {
-    res.json({ res: 'it worked!' });
-})
 
 const defaultTransOptions = {
     returnRawResponse: false,
@@ -25,16 +20,21 @@ const defaultTransOptions = {
     removeStyles: false
 }
 
-app.get('/api/trans', cors(corsOptions), async (req, res) => {
+app.get('/trans', cors(corsOptions), async (req, res) => {
     const { text, from, to } = req.query;
     const result = await translate(text, from, to, defaultTransOptions);
     return res.json(result)
-})
+});
 
-app.get('/api/hear', cors(corsOptions), async (req, res) => {
+
+app.get('/hear', cors(corsOptions), function (req, res) {
     const gtts = new Gtts(req.query.text, req.query.lang);
     gtts.stream().pipe(res);
-})
+});
+
+app.get('/wakeup', cors(corsOptions), function (req, res) {
+    res.json({ res: "ok" });
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
