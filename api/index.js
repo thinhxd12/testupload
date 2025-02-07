f7aa02d684b6a428aeb252aa068ec7bb4bc7db3c
thinhxd12 <86100800+thinhxd12@users.noreply.github.com>
2025-02-07 21:50:40 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 const cors = require('@fastify/cors');
 
-// CORS options
-const corsOptions = {
-  origin: 'https://vocabs1.vercel.app'
-  methods: ['GET'],
-};
-
 const fastify = require('fastify')({
   logger: true
 })
 
-// fastify.register(require('./app'))
-fastify.register(cors, corsOptions, require('./app'));
+const corsOptions = {
+  origin: ['https://vocabs1.vercel.app', 'https://hoctuvung3.vercel.app']
+  methods: ['GET']
+};
+
+fastify.register(cors, corsOptions);
+
+// Register your main app after CORS is set up
+fastify.register(require('./app'));
 
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {
