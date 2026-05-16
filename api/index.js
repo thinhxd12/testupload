679ea49926712753121aff317843f477dc0f9e1a
thinhxd12 <thinhxd12@gmail.com>
2026-05-16 10:02:17 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -1,12 +1,42 @@
-import Fastify from 'fastify'
-
-const app = Fastify({
+const translate = require("google-translate-extended-api");
+const gtts = require("node-gtts");
+const app = require("fastify")({
   logger: true,
-})
+});
 
+const defaultTransOptions = {
+  returnRawResponse: false,
+  detailedTranslations: true,
+  definitionSynonyms: false,
+  detailedTranslationsSynonyms: true,
+  definitions: false,
+  definitionExamples: false,
+  examples: false,
+  removeStyles: false,
+};
 
 app.get("/", async (req, reply) => {
   return reply.status(200).type("application/json").send({ hello: "world" });
+});
+
+app.get("/trans", async (req, reply) => {
+  const { text, from, to } = req.query;
+  try {
+    const result = await translate(text, from, to, defaultTransOptions);
+    return reply.status(200).type("application/json").send(result);
+  } catch (err) {
+    reply.status(500).send({ error: "Failed", details: err.message });
+  }
+});
+
+app.get("/speech", async (req, reply) => {
+  const { text, lang = "en" } = req.query;
+  try {
+    const stream = gtts(lang).stream(text);
+    return reply.send(stream);
+  } catch (err) {
+    reply.status(500).send({ error: "Failed", details: err.message });
+  }
 });
 
 export default async function handler(req, reply) {
