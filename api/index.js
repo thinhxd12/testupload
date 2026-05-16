479563140ac6cfbdf54b40579f42c284d240c717
thinhxd12 <thinhxd12@gmail.com>
2026-05-16 09:56:44 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -1,24 +1,9 @@
 import Fastify from 'fastify'
-import cors from '@fastify/cors'
 
 const app = Fastify({
   logger: true,
 })
 
-const corsOptions = {
-  origin: (origin, cb) => {
-    const hostname = new URL(origin).hostname;
-    if (hostname === "localhost" || hostname === "https://vocabs1.vercel.app") {
-      //  Request from localhost will pass
-      cb(null, true);
-      return;
-    }
-    // Generate an error on other origins, disabling access
-    cb(new Error("Not allowed"), false);
-  },
-};
-
-await app.register(cors, corsOptions);
 
 app.get("/", async (req, reply) => {
   return reply.status(200).type("application/json").send({ hello: "world" });
